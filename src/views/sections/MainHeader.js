31e2c4c332c84176f13489d6a0d7323c3b18d425
Ryan Willis <ryan.willis@hotmail.ca>
2021-06-03 18:44:48 -0400

    added disclaimer, updated server

--- src/views/sections/MainHeader.js
+++ src/views/sections/MainHeader.js
@@ -6,9 +6,7 @@ class MainHeader extends Component {
         return (
       <>
         <div className="position-relative">
-          {/* Hero for FREE version */}
           <section className="section section-hero section-shaped">
-            {/* Background circles */}
             <div className="shape shape-style-1 shape-default">
               <span className="span-150" />
               <span className="span-50" />
@@ -25,27 +23,10 @@ class MainHeader extends Component {
               <div className="col px-0">
                 <Row className="align-items-center justify-content-center">
                   <Col className="text-center" lg="6">
-                    {/*<img*/}
-                    {/*  alt="..."*/}
-                    {/*  className="img-fluid"*/}
-                    {/*  src={require("assets/img/brand/argon-react-white.png")}*/}
-                    {/*  style={{ width: "200px" }}*/}
-                    {/*/>*/}
                     <h1 className="bold text-white">
                       Write With <strong>Tech</strong>
                     </h1>
                     <div className="btn-wrapper mt-5">
-                    {/*  <Button*/}
-                    {/*    className="btn-white btn-icon mb-3 mb-sm-0"*/}
-                    {/*    color="default"*/}
-                    {/*    href="https://www.creative-tim.com/product/argon-design-system-react?ref=adsr-landing-page"*/}
-                    {/*    size="lg"*/}
-                    {/*  >*/}
-                    {/*    <span className="btn-inner--icon mr-1">*/}
-                    {/*      <i className="ni ni-cloud-download-95" />*/}
-                    {/*    </span>*/}
-                    {/*    <span className="btn-inner--text">Download React</span>*/}
-                    {/*  </Button>{" "}*/}
                       <Button
                         className="btn-icon mb-3 mb-sm-0"
                         color="github"
@@ -62,22 +43,10 @@ class MainHeader extends Component {
                         </span>
                       </Button>
                     </div>
-                    {/*<div className="mt-5">*/}
-                    {/*  <small className="text-white font-weight-bold mb-0 mr-2">*/}
-                    {/*    *proudly coded by*/}
-                    {/*  </small>*/}
-                    {/*  <img*/}
-                    {/*    alt="..."*/}
-                    {/*    className="ml-1"*/}
-                    {/*    style={{ height: "28px" }}*/}
-                    {/*    src={require("assets/img/brand/creativetim-white-slim.png")}*/}
-                    {/*  />*/}
-                    {/*</div>*/}
                   </Col>
                 </Row>
               </div>
             </Container>
-            {/* SVG separator */}
             <div className="separator separator-bottom separator-skew zindex-100">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
